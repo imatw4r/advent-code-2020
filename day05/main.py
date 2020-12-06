8e5a70537748a6a0ae3596332ed7e2ea11fd95f1
Bartek Krzys <b.krzys@clearcode.cc>
2020-12-06 19:55:52 +0100

    - Day05 add Algorithm explanation

--- day05/main.py
+++ day05/main.py
@@ -2,6 +2,22 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
+# Algorithm:
+# Given boarding pass:
+#       1. Translate it to binary, where:
+#               F=0, B=1, R=0, L=1
+#       2. Translate binary parts to decimal:
+#           0101010 => 42 (row)
+#           111 => 7 (col)
+#       3. Calculate seat_id using formula:
+#           row * 7 + col
+#
+# Example:
+#     FBFBFBFLRL => 0101010111
+#     0101010    => 42 (row)
+#            101 => 5 (col)
+#     Seat id: 42 * 8 + 5 => 341
+
 TRANS_TABLE = {ord("F"): "0", ord("B"): "1", ord("R"): "1", ord("L"): "0"}
 SEAT_ROW = slice(0, 7)
 SEAT_COL = slice(7, 10)
