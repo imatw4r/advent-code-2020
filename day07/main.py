4386f36b95dcc7aa288d3dff853b4ae9dcf02f17
Bartek Krzys <b.krzys@clearcode.cc>
2020-12-07 17:41:09 +0100

    - Day07 add input example to docstring of function responsible to build bags rule object

--- day07/main.py
+++ day07/main.py
@@ -5,8 +5,8 @@ from typing import Dict, List, Tuple
 
 
 BASE_DIR = os.path.dirname(__file__)
-name_pattern = re.compile(r"^(\w+ \w+)")
-content_pattern = re.compile(r"(\d+) (\w+ \w+|no other bags.)")
+NAME_PATTERN = re.compile(r"^(\w+ \w+)")
+CONTENT_PATTERN = re.compile(r"(\d+) (\w+ \w+|no other bags.)")
 
 BagName = str
 RawBagOccurences = str
@@ -17,21 +17,35 @@ BagRules = Dict[BagName, BagContent]
 
 
 def to_name(line: str) -> str:
-    return re.match(name_pattern, line).group()
+    return re.match(NAME_PATTERN, line).group()
 
 
 def to_content(line: str) -> BagContent:
     return dict(
         [
             (bag_name, int(occurences))
-            for occurences, bag_name in re.findall(content_pattern, line)
+            for occurences, bag_name in re.findall(CONTENT_PATTERN, line)
         ]
     )
 
 
 def build_bag_rules(data: List[str]) -> BagRules:
     """
-    Example output:
+    Input:
+        [
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.",
+            "dotted black bags contain no other bags.",
+        ]
+
+
+    Output:
         {
             "light red": {"bright white": 1, "muted yellow": 2},
             "dark orange": {"bright white": 3, "muted yellow": 4},
